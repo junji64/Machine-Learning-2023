821a64d5a894aeb13f993bdcdf31bf445b36cf0b
Jun Ji <junji64@gmail.com>
2024-06-10 09:48:30 +0900

    Add files via upload

--- C2/week1/C2W1A1/public_tests.py
+++ C2/week1/C2W1A1/public_tests.py
@@ -7,18 +7,22 @@ import numpy as np
 def test_c1(target):
     assert len(target.layers) == 3, \
         f"Wrong number of layers. Expected 3 but got {len(target.layers)}"
-    assert target.input.shape.as_list() == [None, 400], \
-        f"Wrong input shape. Expected [None,  400] but got {target.input.shape.as_list()}"
+#     assert target.input.shape.as_list() == [None, 400], \
+#         f"Wrong input shape. Expected [None,  400] but got {target.input.shape.as_list()}"
+    assert target.input_shape == (None, 400), \
+        f"Wrong input shape. Expected (None, 400) but got {target.input_shape}"
     i = 0
-    expected = [[Dense, [None, 25], sigmoid],
-                [Dense, [None, 15], sigmoid],
-                [Dense, [None, 1], sigmoid]]
+    expected = [[Dense, (None, 25), sigmoid],
+                [Dense, (None, 15), sigmoid],
+                [Dense, (None, 1), sigmoid]]
 
     for layer in target.layers:
         assert type(layer) == expected[i][0], \
             f"Wrong type in layer {i}. Expected {expected[i][0]} but got {type(layer)}"
-        assert layer.output.shape.as_list() == expected[i][1], \
-            f"Wrong number of units in layer {i}. Expected {expected[i][1]} but got {layer.output.shape.as_list()}"
+#         assert layer.output.shape.as_list() == expected[i][1], \
+#             f"Wrong number of units in layer {i}. Expected {expected[i][1]} but got {layer.output.shape.as_list()}"
+        assert layer.output_shape == expected[i][1], \
+            f"Wrong number of units in layer {i}. Expected {expected[i][1]} but got {layer.output_shape}"
         assert layer.activation == expected[i][2], \
             f"Wrong activation in layer {i}. Expected {expected[i][2]} but got {layer.activation}"
         i = i + 1
